8446f569c5ab9725e797ad43473772cb6db33a54
wgehalo <wgehalo@gmail.com>
2019-01-11 15:03:55 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args[0] == "yay") {
+                Console.WriteLine("you did it!");
+            }
+
+            else
+            {
+                Console.WriteLine("invalid..");
+            }
+        }
+    }
+}
